604beccb68f302fae0e5ee9bd57063a8eb7a0804
Patrik Karisch <patrik@karisch.guru>
2015-08-01 13:26:09 +0200

    Some CS fixes and version bump

--- fileadmin/app/AppKernel.php
+++ fileadmin/app/AppKernel.php
@@ -2,13 +2,21 @@
 
 use Bartacus\Bundle\BartacusBundle\Kernel\Kernel;
 
-class AppKernel extends Kernel {
+/**
+ * The AppKernel is the central part of your symfony part
+ */
+class AppKernel extends Kernel
+{
 
-	public function registerBundles() {
-		$bundles = array(
-			// Put here your own bundles/extensions!
-		);
+    /**
+     * {@inheritDoc}
+     */
+    public function registerBundles()
+    {
+        $bundles = [
+            // Put here your own bundles/extensions!
+        ];
 
-		return array_merge(parent::registerBundles(), $bundles);
-	}
+        return array_merge(parent::registerBundles(), $bundles);
+    }
 }
