2ff4887c02bb0bd9066740441c83fef9876d6425
Patrik Karisch <patrik@karisch.guru>
2015-08-08 15:09:44 +0200

    Use stable version of Bartacus and update autoloading for annotations

--- typo3conf/AdditionalConfiguration.php
+++ typo3conf/AdditionalConfiguration.php
@@ -1,10 +1,20 @@
 <?php
 
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Core\Bootstrap;
+
+$loader = Bootstrap::getInstance()->getEarlyInstance('Composer\\Autoload\\ClassLoader');
+AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+
 require_once PATH_site.'fileadmin/app/AppKernel.php';
 
-use TYPO3\CMS\Core\Utility\GeneralUtility;
+$environment = (string)GeneralUtility::getApplicationContext();
+$environmentCleaned = str_replace('/', '', $environment);
 
-$context = GeneralUtility::getApplicationContext();
+if(file_exists(PATH_site.'typo3conf/AdditionalConfiguration'.$environmentCleaned.'.php')) {
+    require PATH_site.'typo3conf/AdditionalConfiguration'.$environmentCleaned.'.php';
+}
 
-$kernel = new AppKernel((string)$context, $context->isDevelopment());
+$kernel = new AppKernel($environment, GeneralUtility::getApplicationContext()->isDevelopment());
 $kernel->boot();
