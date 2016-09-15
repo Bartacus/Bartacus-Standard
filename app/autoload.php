4fa366171107d5cb856ea9afc3f428e136d31f3b
Patrik Karisch <patrik@karisch.guru>
2016-09-15 15:55:31 +0200

    Add a reduced symfony standard installation

---
+++ app/autoload.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Composer\Autoload\ClassLoader;
+
+/** @var ClassLoader $loader */
+$loader = require __DIR__.'/../vendor/autoload.php';
+
+AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+
+return $loader;
