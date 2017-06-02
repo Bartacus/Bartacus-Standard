77b1aa071e44c68da7d0a1258cadf09fef096dea
Patrik Karisch <patrik@karisch.guru>
2017-06-02 11:07:32 +0200

    Add useful debug and logging settings in Development context

--- web/typo3conf/AdditionalConfiguration.php
+++ web/typo3conf/AdditionalConfiguration.php
@@ -4,6 +4,18 @@ declare(strict_types = 1);
 
 use Bartacus\Bundle\BartacusBundle\Config\ConfigLoader;
 use Symfony\Component\HttpKernel\Kernel;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+if (GeneralUtility::getApplicationContext()->isDevelopment()) {
+    $GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = true;
+    $GLOBALS['TYPO3_CONF_VARS']['BE']['sessionTimeout'] = 31536000; // One year!
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = true;
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['sqlDebug'] = 1;
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = 1;
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['systemLogLevel'] = 1;
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['enableDeprecationLog'] = 1;
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['clearCacheSystem'] = true;
+}
 
 $kernel = $GLOBALS['kernel'];
 if ($kernel instanceof Kernel) {
